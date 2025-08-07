511a2c7fe6578a4d43ee795fb502719d0e355a40
Rodrigo <rodrigoperezserrano28@gmail.com>
2025-08-07 21:06:31 +0200

    [first-commit] Disable line

--- src/pages/admin/ListaPreguntas.jsx
+++ src/pages/admin/ListaPreguntas.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "./ListaPreguntas.module.css"; // puedes crearlo o quitarlo
+// import styles from "./ListaPreguntas.module.css"; // puedes crearlo o quitarlo
 // import Loader from "../../components/Loader"; // opcional si tienes uno
 
 function ListaPreguntas() {
